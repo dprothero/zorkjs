fbc13f03c7e747046af60c20e08db74324c8d6a0
David Prothero <david@prothero.com>
2019-07-12 12:32:42 -0700

    Use tmpdir (#1)

--- zork.js
+++ zork.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const fs = require('fs');
+const tmpdir = require('os').tmpdir();
+const path = require('path');
 const AdmZip = require('adm-zip');
 const GlkOte = require('glkote-term');
 const ZVM = require('ifvms').ZVM;
@@ -20,19 +22,20 @@ const download = function(url, dest, cb) {
 };
 
 const getZorkDat = function(cb) {
-  download('http://www.infocom-if.org/downloads/zork1.zip', 'z.zip', err => {
+  const localZip = path.join(tmpdir, 'z.zip');
+  download('http://www.infocom-if.org/downloads/zork1.zip', localZip, err => {
     if (err) {
       cb(err);
     } else {
-      const zip = new AdmZip('z.zip');
-      zip.extractEntryTo('DATA/ZORK1.DAT', '.', false, true);
-      fs.unlinkSync('z.zip');
+      const zip = new AdmZip(localZip);
+      zip.extractEntryTo('DATA/ZORK1.DAT', tmpdir, false, true);
+      fs.unlinkSync(localZip);
       cb();
     }
   });
 };
 
-const runZork = function() {
+const runZork = function(datFile) {
   const vm = new ZVM();
   const Glk = GlkOte.Glk;
 
@@ -43,23 +46,24 @@ const runZork = function() {
     GlkOte: new GlkOte()
   };
 
-  vm.prepare(fs.readFileSync('ZORK1.DAT'), options);
+  vm.prepare(fs.readFileSync(datFile), options);
 
   // This will call vm.init()
   Glk.init(options);
 };
 
 const launchZork = function() {
-  if (!fs.existsSync('ZORK1.DAT')) {
+  const datFile = path.join(tmpdir, 'ZORK1.DAT');
+  if (!fs.existsSync(datFile)) {
     getZorkDat(err => {
       if (err) {
         console.error(err);
       } else {
-        runZork();
+        runZork(datFile);
       }
     });
   } else {
-    runZork();
+    runZork(datFile);
   }
 };
 
